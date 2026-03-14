456aa2eec67705781ee60b96b774a2abaea5b5dc
Azka <azkadaiki26@gmail.com>
2026-03-14 23:27:17 +0700

    feat: add db and auth dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import openapi from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(openapi())
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
